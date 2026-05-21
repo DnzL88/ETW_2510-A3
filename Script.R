f29aa8b97ccd642534b8881d6e35c7be403defb8
MonDkhu <dkhu0006@student.monash.edu>
2026-05-21 18:50:21 +0800

    Update Script.R

--- Script.R
+++ Script.R
@@ -4,19 +4,33 @@ install.packages("tseries")
 library(readxl)
 library(dplyr)
 library(tseries)
-data <- read_excel("ETW2510_A3 dataset.xlsx", sheet = "Time-Series Data")
+library(tidyverse)
+data <- read_excel("ETW2510_A3 dataset change.xlsx", sheet = "Time-Series Data")
 head(data)
 str(data)
 data_clean <- data %>%
   select(date, DI, GDP, UNEMP, PCE, TRANSFER)
 head(data_clean)
 summary(data_clean)
+
+#Histogram for variables
+hist(data$DI)
+hist(data$GDP)
+hist(data$UNEMP)
+hist(data$PCE)
+hist(data$TRANSFER)
+
 colSums(is.na(data_clean))
 DI_ts <- ts(data_clean$DI, frequency = 4, start = c(1951, 4))
+DI_ts <- log(DI_ts)
 GDP_ts <- ts(data_clean$GDP, frequency = 4, start = c(1951, 4))
 UNEMP_ts <- ts(data_clean$UNEMP, frequency = 4, start = c(1951, 4))
+UNEMP_ts <-log(UNEMP_ts)
 PCE_ts <- ts(data_clean$PCE, frequency = 4, start = c(1951, 4))
+PCE_ts <- log(PCE_ts)
 TRANSFER_ts <- ts(data_clean$TRANSFER, frequency = 4, start = c(1951, 4))
+TRANSFER_ts <- log(TRANSFER_ts)
+
 
 # ADF tests at level
 adf.test(DI_ts)
@@ -36,3 +50,10 @@ adf.test(d_DI)
 adf.test(d_GDP)
 adf.test(d_PCE)
 adf.test(d_TRANSFER)
+
+hist(data$DI)
+hist(data$GDP)
+hist(data$UNEMP)
+hist(data$PCE)
+hist(data$TRANSFER)
+
