47696391ec0feecb1c21648b2da85a24619bb39a
Skylar-Ran <cuixiaoran21@gmail.com>
2026-05-21 19:11:42 +0800

    install.packages("readxl") install.packages("dplyr") install.packages("tseries") install.packages("ggplot2") library(readxl) library(dplyr) library(tseries) library(ggplot2) data <- read_excel("ETW2510_A3 dataset.xlsx", sheet = "Time-Series Data") head(data) str(data) data_clean <- data %>%   select(date, DI, GDP, UNEMP, PCE, TRANSFER) head(data_clean) summary(data_clean) colSums(is.na(data_clean)) DI_ts <- ts(data_clean$DI, frequency = 4, start = c(1951, 4)) GDP_ts <- ts(data_clean$GDP, frequency = 4, start = c(1951, 4)) UNEMP_ts <- ts(data_clean$UNEMP, frequency = 4, start = c(1951, 4)) PCE_ts <- ts(data_clean$PCE, frequency = 4, start = c(1951, 4)) TRANSFER_ts <- ts(data_clean$TRANSFER, frequency = 4, start = c(1951, 4))
    adf.test(DI_ts)
    adf.test(GDP_ts)
    adf.test(UNEMP_ts)
    adf.test(PCE_ts)
    adf.test(TRANSFER_ts)
    
    d_DI <- diff(DI_ts)
    d_GDP <- diff(GDP_ts)
    d_PCE <- diff(PCE_ts)
    d_TRANSFER <- diff(TRANSFER_ts)
    
    adf.test(d_DI)
    adf.test(d_GDP)
    adf.test(d_PCE)
    adf.test(d_TRANSFER)
    
    pp.test(DI_ts)
    pp.test(GDP_ts)
    pp.test(UNEMP_ts)
    pp.test(PCE_ts)
    pp.test(TRANSFER_ts)
    
    pp.test(d_DI)
    pp.test(d_GDP)
    pp.test(d_PCE)
    pp.test(d_TRANSFER)
    
    ggplot(data_clean, aes(x = date, y = DI)) +
      geom_line() +
      labs(title = "Disposable Income over Time", x = "Date", y = "DI")
    
    ggplot(data_clean, aes(x = date, y = GDP)) +
      geom_line() +
      labs(title = "GDP over Time", x = "Date", y = "GDP")
    
    ggplot(data_clean, aes(x = date, y = UNEMP)) +
      geom_line() +
      labs(title = "Unemployment Rate over Time", x = "Date", y = "UNEMP")
    
    ggplot(data_clean, aes(x = date, y = PCE)) +
      geom_line() +
      labs(title = "Personal Consumption Expenditure over Time", x = "Date", y = "PCE")
    
    ggplot(data_clean, aes(x = date, y = TRANSFER)) +
      geom_line() +
      labs(title = "Federal Government Transfer over Time", x = "Date", y = "TRANSFER")

--- Script.R
+++ Script.R
@@ -1,36 +1,24 @@
 install.packages("readxl")
 install.packages("dplyr")
 install.packages("tseries")
+install.packages("ggplot2")
 library(readxl)
 library(dplyr)
 library(tseries)
-library(tidyverse)
-data <- read_excel("ETW2510_A3 dataset change.xlsx", sheet = "Time-Series Data")
+library(ggplot2)
+data <- read_excel("ETW2510_A3 dataset.xlsx", sheet = "Time-Series Data")
 head(data)
 str(data)
 data_clean <- data %>%
   select(date, DI, GDP, UNEMP, PCE, TRANSFER)
 head(data_clean)
 summary(data_clean)
-
-#Histogram for variables
-hist(data$DI)
-hist(data$GDP)
-hist(data$UNEMP)
-hist(data$PCE)
-hist(data$TRANSFER)
-
 colSums(is.na(data_clean))
 DI_ts <- ts(data_clean$DI, frequency = 4, start = c(1951, 4))
-DI_ts <- log(DI_ts)
 GDP_ts <- ts(data_clean$GDP, frequency = 4, start = c(1951, 4))
 UNEMP_ts <- ts(data_clean$UNEMP, frequency = 4, start = c(1951, 4))
-UNEMP_ts <-log(UNEMP_ts)
 PCE_ts <- ts(data_clean$PCE, frequency = 4, start = c(1951, 4))
-PCE_ts <- log(PCE_ts)
 TRANSFER_ts <- ts(data_clean$TRANSFER, frequency = 4, start = c(1951, 4))
-TRANSFER_ts <- log(TRANSFER_ts)
-
 
 # ADF tests at level
 adf.test(DI_ts)
@@ -51,9 +39,36 @@ adf.test(d_GDP)
 adf.test(d_PCE)
 adf.test(d_TRANSFER)
 
-hist(data$DI)
-hist(data$GDP)
-hist(data$UNEMP)
-hist(data$PCE)
-hist(data$TRANSFER)
+# PP tests at level as robustness check
+pp.test(DI_ts)
+pp.test(GDP_ts)
+pp.test(UNEMP_ts)
+pp.test(PCE_ts)
+pp.test(TRANSFER_ts)
 
+# PP tests at first difference as robustness check
+pp.test(d_DI)
+pp.test(d_GDP)
+pp.test(d_PCE)
+pp.test(d_TRANSFER)
+
+# Plot selected variables over time
+ggplot(data_clean, aes(x = date, y = DI)) +
+  geom_line() +
+  labs(title = "Disposable Income over Time", x = "Date", y = "DI")
+
+ggplot(data_clean, aes(x = date, y = GDP)) +
+  geom_line() +
+  labs(title = "GDP over Time", x = "Date", y = "GDP")
+
+ggplot(data_clean, aes(x = date, y = UNEMP)) +
+  geom_line() +
+  labs(title = "Unemployment Rate over Time", x = "Date", y = "UNEMP")
+
+ggplot(data_clean, aes(x = date, y = PCE)) +
+  geom_line() +
+  labs(title = "Personal Consumption Expenditure over Time", x = "Date", y = "PCE")
+
+ggplot(data_clean, aes(x = date, y = TRANSFER)) +
+  geom_line() +
+  labs(title = "Federal Government Transfer over Time", x = "Date", y = "TRANSFER")
